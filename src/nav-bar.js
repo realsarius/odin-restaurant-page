71986629be18c8241b4e83f14a016c3a8061cfcd
realsarius <54468228+realsarius@users.noreply.github.com>
2023-02-09 09:07:33 +0300

    completed the project

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -4,25 +4,25 @@ const navBarLeft = document.createElement('div');
 const navBarRight = document.createElement('div');
 const navBarRightUList = document.createElement('ul');
 
-const navBarRightUList1 = document.createElement('li');
+// const navBarRightUList1 = document.createElement('li');
 const navBarRightUList2 = document.createElement('li');
 const navBarRightUList3 = document.createElement('li');
 const navBarRightUList4 = document.createElement('li');
-const navBarRightUList5 = document.createElement('li');
+// const navBarRightUList5 = document.createElement('li');
 
 // Links
-const navBarRightUList1a = document.createElement('a');
+// const navBarRightUList1a = document.createElement('a');
 const navBarRightUList2a = document.createElement('a');
 const navBarRightUList3a = document.createElement('a');
 const navBarRightUList4a = document.createElement('a');
-const navBarRightUList5a = document.createElement('a');
+// const navBarRightUList5a = document.createElement('a');
 const navBarLefta = document.createElement('a');
 
-navBarRightUList1a.href = '#';
+// navBarRightUList1a.href = '#';
 navBarRightUList2a.href = '#';
 navBarRightUList3a.href = '#';
 navBarRightUList4a.href = '#';
-navBarRightUList5a.href = '#';
+// navBarRightUList5a.href = '#';
 
 navBarLefta.href = '#';
 
@@ -31,23 +31,23 @@ navBarLeft.className = 'logo';
 
 navBarLeft.appendChild(navBarLefta);
 
-navBarRightUList1a.textContent = 'Order Now';
+// navBarRightUList1a.textContent = 'Order Now';
 navBarRightUList2a.textContent = 'Home';
 navBarRightUList3a.textContent = 'Gallery';
 navBarRightUList4a.textContent = 'Story';
-navBarRightUList5a.textContent = 'Visit';
+// navBarRightUList5a.textContent = 'Visit';
 
-navBarRightUList1.appendChild(navBarRightUList1a);
+// navBarRightUList1.appendChild(navBarRightUList1a);
 navBarRightUList2.appendChild(navBarRightUList2a);
 navBarRightUList3.appendChild(navBarRightUList3a);
 navBarRightUList4.appendChild(navBarRightUList4a);
-navBarRightUList5.appendChild(navBarRightUList5a);
+// navBarRightUList5.appendChild(navBarRightUList5a);
 
-navBarRightUList.appendChild(navBarRightUList1);
+// navBarRightUList.appendChild(navBarRightUList1);
 navBarRightUList.appendChild(navBarRightUList2);
 navBarRightUList.appendChild(navBarRightUList3);
 navBarRightUList.appendChild(navBarRightUList4);
-navBarRightUList.appendChild(navBarRightUList5);
+// navBarRightUList.appendChild(navBarRightUList5);
 
 navBarRight.appendChild(navBarRightUList);
 
@@ -57,9 +57,9 @@ navBar.appendChild(navBarRight);
 export {
   navBar,
   navBarLefta,
-  navBarRightUList1a,
+  // navBarRightUList1a,
   navBarRightUList2a,
   navBarRightUList3a,
   navBarRightUList4a,
-  navBarRightUList5a,
+  // navBarRightUList5a,
 };
