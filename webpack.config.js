10ba147bc4ee0f572e0e807485a7e3f40f62ba8c
realsarius <54468228+realsarius@users.noreply.github.com>
2023-02-18 22:22:39 +0300

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,20 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-transform-arrow-functions',
+            ],
+          },
+        },
+      },
     ],
   },
 };
